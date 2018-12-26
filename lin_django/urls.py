e6e0ae70eb211858371e4257f1e55d7dd38e844d
lintangwisesa <lintangwisesa@ymail.com>
2018-12-26 13:51:37 +0700

    django mysql

--- lin_django/urls.py
+++ lin_django/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('posts/', include('posts.urls')),
     path('lin_app/', include('lin_app.urls')),
     path('admin/', admin.site.urls),
 ]
