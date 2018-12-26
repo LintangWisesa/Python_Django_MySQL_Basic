27bde4448f77a696311d6484b70094e498191762
lintangwisesa <lintangwisesa@ymail.com>
2018-12-26 13:57:32 +0700

    django mysql

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Selamat datang di posts!")
+    # return HttpResponse("Selamat datang di posts!")
+    return render(request, 'posts/index.html')
