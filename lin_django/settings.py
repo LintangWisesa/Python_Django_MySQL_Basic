8cbf931ad837e4e9c7796999024e91f989fcd604
lintangwisesa <lintangwisesa@ymail.com>
2018-12-26 11:39:58 +0700

    django mysql

--- lin_django/settings.py
+++ lin_django/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'lin_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lin_django',
+        'USER': 'lintang',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
