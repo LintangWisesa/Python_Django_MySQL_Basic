0619ad08bf22fc11eedb7ccbfd51ecc8b6761a0f
lintangwisesa <lintangwisesa@ymail.com>
2018-12-26 14:34:30 +0700

    django mysql

--- posts/models.py
+++ posts/models.py
@@ -5,4 +5,9 @@ class Posts(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(default = datetime.now, blank = True)
-    
+    # untuk membuat judul tiap post = title-nya:
+    def __str__(self):
+        return self.title
+    # untuk membuat nama plural custom utk model kita
+    class Meta:
+        verbose_name_plural = "Posts"
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 def index(request):
+    # // menampilkan Teks selamat datang:
     # return HttpResponse("Selamat datang di posts!")
+    # // menampilkan file template layout html:
     return render(request, 'posts/index.html')
+
+    # posts = Posts.objects.all()[:10]
+    # context = {
+    #     'title': 'Latest Posts',
+    #     'posts': posts
+    # }
+    # return HttpResponse(context)
